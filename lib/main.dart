8df2c64b724dfc0cef450a2b242a6d3d1c223dfc
Saad Saleem <msaadsaleem1001@gmail.com>
2024-03-19 09:54:22 +0500

    Added Freezed Example.

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:bloc_tutorials/Bloc/Todo%20Bloc/todo_bloc.dart';
 import 'package:bloc_tutorials/Bloc/posts_bloc/post_bloc.dart';
 import 'package:bloc_tutorials/Freezed%20Example/Freezed%20UI/freezed_ui.dart';
 import 'package:bloc_tutorials/Repositories/SQLITE%20DB/sqlite_db_repo.dart';
-// import 'package:bloc_tutorials/UI/ToDo%20Screen/todo_screen.dart';
 import 'package:bloc_tutorials/Utils/Image%20Picker%20Utils/image_picker_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
